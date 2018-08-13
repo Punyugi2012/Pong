3a48fc7b5ee158a884229344ea6534141728758f
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-13 18:24:48 +0700

    update font

--- Pong/Scenes/GamePlay/GameScene.swift
+++ Pong/Scenes/GamePlay/GameScene.swift
@@ -18,10 +18,20 @@ class GameScene: SKScene {
     var readyTimeLabel: SKLabelNode?
     var scoreEnemyLabel: SKLabelNode?
     var scorePlayerLabel: SKLabelNode?
+    var replay: SKSpriteNode?
+    var home: SKSpriteNode?
+    var modal: SKSpriteNode?
+    var labelInModal: SKLabelNode?
     var score = [0, 0]
     var countTime = 3
     
     override func didMove(to view: SKView) {
+//        for fontFamily in UIFont.familyNames {
+//            print("Font family name = \(fontFamily as String)")
+//            for fontName in UIFont.fontNames(forFamilyName: fontFamily as String) {
+//                print("- Font name = \(fontName)")
+//            }
+//        }
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0
         border.restitution = 1
@@ -32,10 +42,21 @@ class GameScene: SKScene {
         readyTimeLabel = childNode(withName: "ReadyTime") as? SKLabelNode
         scoreEnemyLabel = childNode(withName: "ScoreEnemy") as? SKLabelNode
         scorePlayerLabel = childNode(withName: "ScorePlayer") as? SKLabelNode
+        modal = childNode(withName: "Modal") as? SKSpriteNode
+        labelInModal = modal?.childNode(withName: "Label") as? SKLabelNode
+        modal?.alpha = 0
+        replay = modal?.childNode(withName: "Replay") as? SKSpriteNode
+        home = modal?.childNode(withName: "Home") as? SKSpriteNode
         physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
+        setFont()
         prepareStartGame()
     }
-    
+    private func setFont() {
+        readyTimeLabel?.fontName = "OrangeKid-Regular"
+        scorePlayerLabel?.fontName = "OrangeKid-Regular"
+        scoreEnemyLabel?.fontName = "OrangeKid-Regular"
+        labelInModal?.fontName = "OrangeKid-Regular"
+    }
     private func updateScore(who: SKLabelNode?) {
         if let who = who {
             if who == scorePlayerLabel {
@@ -103,7 +124,13 @@ class GameScene: SKScene {
     }
     
     private func showModal() {
-        
+        modal?.alpha = 1
+        if score[0] == 5 {
+            labelInModal?.text = "Player1 Won!"
+        }
+        else if score[1] == 5 {
+            labelInModal?.text = "Player2 Won!"
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -119,14 +146,34 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let location = touches.first?.location(in: self) {
+        for touch in touches {
+            let location = touch.location(in: self)
             player?.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            if atPoint(location).name == "Replay" {
+                if let gameScene = GameScene(fileNamed: "GameScene") {
+                    gameScene.scaleMode = .aspectFill
+                    view!.presentScene(gameScene)
+                }
+            }
+            if atPoint(location).name == "Home" {
+                
+            }
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let location = touches.first?.location(in: self) {
+        for touch in touches {
+            let location = touch.location(in: self)
             player?.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            if atPoint(location).name == "Replay" {
+                if let gameScene = GameScene(fileNamed: "GameScene") {
+                    gameScene.scaleMode = .aspectFill
+                    view!.presentScene(gameScene)
+                }
+            }
+            if atPoint(location).name == "Home" {
+                
+            }
         }
     }
     
